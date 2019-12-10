0436f87c41c1d445f9279cf7f5827539d56b21c5
Cedric Bourneville <cbourneville@gmail.com>
2019-12-10 15:30:06 +1300

    7: Implementing Intcode computer

---
+++ 2019/IntCode Computer/IntCode Computer/IntcodeComputer.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntCode_Computer
+{
+	public class IntcodeComputer
+	{
+		private int[] _program;
+		private int _programCounter;
+		private Queue<int> _inputs;
+		private Queue<int> _outputs;
+
+		public IntcodeComputer(int[] program, int? phase = null)
+		{
+			_program = program;
+			_programCounter = 0;
+			_inputs = new Queue<int>();
+			_outputs = new Queue<int>();
+
+			if (phase.HasValue)
+			{
+				_inputs.Enqueue(phase.Value);
+			}
+		}
+
+		public void QueueInput(int input)
+		{
+			_inputs.Enqueue(input);
+		}
+
+		public int GetOutput()
+		{
+			return _outputs.Dequeue();
+		}
+
+		public void Run()
+		{
+			while (_programCounter < _program.Length)
+			{
+				var instruction = _program[_programCounter];
+
+				switch (instruction % 100)
+				{
+					case 1:
+						one();
+						break;
+					case 2:
+						two();
+						break;
+					case 3:
+						if (_inputs.Count > 0)
+							three();
+						break;
+					case 4:
+						four();
+						break;
+					case 5:
+						five();
+						break;
+					case 6:
+						six();
+						break;
+					case 7:
+						seven();
+						break;
+					case 8:
+						eight();
+						break;
+					case 99:
+						_programCounter = _program.Length + 1;
+						break;
+				}
+			}
+
+		}
+
+		int Mode => _program[_programCounter] / 100;
+
+		int Var1 => _program[_programCounter + 1];
+		int Var2 => _program[_programCounter + 2];
+		int Var3 => _program[_programCounter + 3];
+
+		private void one()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+			int param2 = (Mode & 10) > 0 ? Var2 : _program[Var2];
+
+			_program[Var3] = param1 + param2;
+
+			_programCounter += 4;
+
+			return;
+		}
+
+		private void two()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+			int param2 = (Mode & 10) > 0 ? Var2 : _program[Var2];
+
+			_program[Var3] = param1 * param2;
+
+			_programCounter += 4;
+
+			return;
+		}
+
+		private void three()
+		{
+			_program[Var1] = _inputs.Dequeue();
+
+			_programCounter += 2;
+
+			return;
+		}
+
+		private void four()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+
+			_outputs.Enqueue(param1);
+
+			_programCounter += 2;
+
+			return;
+		}
+
+		private void five()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+			int param2 = (Mode & 10) > 0 ? Var2 : _program[Var2];
+
+			_programCounter = param1 != 0
+				? param2
+				: _programCounter + 3;
+			return;
+		}
+
+		private void six()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+			int param2 = (Mode & 10) > 0 ? Var2 : _program[Var2];
+
+			_programCounter = param1 == 0
+				? param2
+				: _programCounter + 3;
+			return;
+		}
+
+		private void seven()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+			int param2 = (Mode & 10) > 0 ? Var2 : _program[Var2];
+
+			_program[Var3] = param1 < param2 ? 1 : 0;
+
+			_programCounter += 4;
+
+			return;
+		}
+
+		private void eight()
+		{
+			int param1 = (Mode & 1) > 0 ? Var1 : _program[Var1];
+			int param2 = (Mode & 10) > 0 ? Var2 : _program[Var2];
+
+			_program[Var3] = param1 == param2 ? 1 : 0;
+
+			_programCounter += 4;
+
+			return;
+		}
+	}
+}
---
+++ 2019/IntCode Computer/IntCode Computer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace IntCode_Computer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = System.IO.File.ReadAllText(@"C:\Users\cBourneville\Code\AdventOfCode\2019\Inputs\5.txt");
+			//var input = @"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+			var program = input.Split(',').Select(x => int.Parse(x)).ToArray();
+
+			IntcodeComputer computer = new IntcodeComputer(program);
+
+			computer.QueueInput(1);
+
+			computer.Run();
+
+			var output = computer.GetOutput();
+
+			Console.WriteLine(output);
+		}
+	}
+}
