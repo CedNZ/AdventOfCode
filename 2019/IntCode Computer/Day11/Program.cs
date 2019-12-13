92e8a356d2c75c7a4d269aa542b7c86d2833a97b
Ced <cbourneville@gmail.com>
2019-12-13 23:48:35 +1300

    11: Add code to print out painting

--- 2019/IntCode Computer/Day11/Program.cs
+++ 2019/IntCode Computer/Day11/Program.cs
@@ -20,11 +20,20 @@ namespace Day11
 
 			Dictionary<(int x, int y), Colour> hull = new Dictionary<(int x, int y), Colour>();
 
+			bool firstRun = true;
+			Colour initialColour = Colour.White;
+
 			while (!robot.Halted)
 			{
 				var currentSpaceColour = hull.ContainsKey((robot.X, robot.Y)) 
 					? hull[(robot.X, robot.Y)]
 					: Colour.Black;
+
+				if (firstRun)
+				{
+					currentSpaceColour = initialColour;
+					firstRun = false;
+				}
 
 				robot.QueueInput((int)currentSpaceColour);
 
@@ -45,8 +54,42 @@ namespace Day11
 				robot.Move(directionToTurn == 1);
 			}
 
+
+			int minX = hull.Keys.Min(p => p.x);
+			int maxX = hull.Keys.Max(p => p.x);
+			int minY = hull.Keys.Min(p => p.y);
+			int maxY = hull.Keys.Max(p => p.y);
+
+			Console.WriteLine($"minX: {minX}, minY: {minY}\nmaxX: {maxX}, maxY: {maxY}");
+
+			Colour[,] painting = new Colour[maxY + 1, maxX + 1];
+			foreach(var position in hull.Keys)
+			{
+				painting[position.y, position.x] = hull[position];
+			}
+
+			for (int y = minY; y < maxY; y++)
+			{
+				string row = "";
+				for (int x = minX; x < maxX; x++)
+				{
+					if (painting[y, x] is Colour colour)
+					{
+						row += colour == Colour.Black ? "█" : " ";
+					}
+					else
+					{
+						row += "█";
+					}
+				}
+				Console.WriteLine(row);
+			}
+
+
 			stopwatch.Stop();
-			Console.WriteLine($"Squares painted: {hull.Count}\nElapsed: {stopwatch.Elapsed}");
+			Console.WriteLine($"Squares painted: {hull.Count}\nElapsed: {stopwatch.Elapsed}\n\n");	
+			
+			
 		}
 	}
 
