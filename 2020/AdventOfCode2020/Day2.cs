cc6f821cfb5393a85c3276d291d09b03d5a23b1b
Ced <cbourneville@gmail.com>
2020-12-03 20:26:50 +1300

    Day 2 working, and input

---
+++ 2020/AdventOfCode2020/Day2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020
+{
+    public class Day2 : IDay<(int min, int max, char letter, string password)>
+    {
+        public List<(int min, int max, char letter, string password)> SetupInputs(string[] inputs)
+        {
+            string pattern = @"(\d+)-(\d+) (\w): (\w+)";
+
+            List<(int min, int max, char letter, string password)> result = new List<(int min, int max, char letter, string password)>(inputs.Count());
+
+            foreach(var input in inputs)
+            {
+                var matches = Regex.Matches(input, pattern);
+
+                var min = int.Parse(matches.First().Groups[1].Value);
+                var max = int.Parse(matches.First().Groups[2].Value);
+                var letter = matches.First().Groups[3].Value[0];
+                var password = matches.First().Groups[4].Value;
+
+                result.Add((min, max, letter, password));
+            }
+            return result;
+        }
+
+        public int A(List<(int min, int max, char letter, string password)> inputs)
+        {
+            int validPasswords = 0;
+            foreach(var input in inputs)
+            {
+                int letterCount = input.password.Count(x => x == input.letter);
+                if (letterCount >= input.min && letterCount <= input.max)
+                {
+                    validPasswords++;
+                }
+            }
+            return validPasswords;
+        }
+
+        public int B(List<(int min, int max, char letter, string password)> inputs)
+        {
+            return -1;
+        }
+    }
+}
