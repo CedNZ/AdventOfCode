1cdc524b4e5c4e14ce589eba2d128cb1128388a9
Ced <cbourneville@gmail.com>
2020-12-03 18:15:56 +1300

    Day 1 part A, and input

---
+++ 2020/AdventOfCode2020/Day1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public class Day1 : IDay
+    {
+        public int A(List<int> inputs)
+        {
+            for(int i = 0; i < inputs.Count(); i++)
+            {
+                for(int j = i + 1; j < inputs.Count(); j++)
+                {
+                    if(inputs[i] + inputs[j] == 2020)
+                    {
+                        return inputs[i] * inputs[j];
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int B(List<int> inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
