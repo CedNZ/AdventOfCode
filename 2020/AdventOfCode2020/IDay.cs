f31215ff93d2f302884f45822ff814068c6aac68
Ced <cbourneville@gmail.com>
2020-12-03 18:14:39 +1300

    Setting up main program, plus input handling

---
+++ 2020/AdventOfCode2020/IDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public interface IDay
+    {
+        public int A(List<int> inputs);
+        public int B(List<int> inputs);
+    }
+}
---
+++ 2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Advent of Code 2020");
+            Console.WriteLine("Enter day number: ");
+            var dayNum = int.Parse(Console.ReadLine());
+
+            IDay day = dayNum switch
+            {
+                1 => new Day1(),
+                _ => null
+            };
+
+            var inputs = System.IO.File.ReadAllLines($"..\\..\\..\\Inputs\\day{dayNum}.txt").Select(int.Parse).ToList();
+
+            var outputA = day.A(inputs);
+
+
+            Console.WriteLine($"Day {dayNum} Answer A: {outputA}");
+            Console.Read();
+        }
+    }
+}
