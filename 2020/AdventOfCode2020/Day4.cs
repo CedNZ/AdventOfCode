28c5cea7e9394a46d1ec3b47e90455b1c9fa0cf3
Ced <cbourneville@gmail.com>
2020-12-05 12:21:43 +1300

    Add Func's for calculating Day4 part 2, and unit tests

--- 2020/AdventOfCode2020/Day4.cs
+++ 2020/AdventOfCode2020/Day4.cs
@@ -55,14 +55,106 @@ namespace AdventOfCode2020
             return passports;
         }
 
+        Func<Passport, bool> FieldsPresentFunc = (p) =>
+        {
+            return !string.IsNullOrWhiteSpace(p.byr)
+            && !string.IsNullOrWhiteSpace(p.iyr)
+            && !string.IsNullOrWhiteSpace(p.eyr)
+            && !string.IsNullOrWhiteSpace(p.hgt)
+            && !string.IsNullOrWhiteSpace(p.hcl)
+            && !string.IsNullOrWhiteSpace(p.ecl)
+            && !string.IsNullOrWhiteSpace(p.pid);
+        };
+
         public long A(List<Passport> inputs)
         {
-            return inputs.Count(p => p.IsValid);
+            return inputs.Count(FieldsPresentFunc);
         }
+
+        public static Func<Passport, bool> BirthYearValid = (p) => YearValid(p.byr, 1920, 2002);
+        public static Func<Passport, bool> IssueYearValid = (p) => YearValid(p.iyr, 2010, 2020);
+        public static Func<Passport, bool> ExpiryYearValid = (p) => YearValid(p.eyr, 2020, 2030);
+
+        public static Func<string, int, int, bool> YearValid = (yearString, minYear, maxYear) =>
+        {
+            if (int.TryParse(yearString, out int year))
+            {
+                return year >= minYear && year <= maxYear;
+            }
+            return false;
+        };
+
+        public static Func<Passport, bool> HeightValid = (p) =>
+        {
+            var unit = p.hgt.Substring(p.hgt.Length - 2);
+            var number = p.hgt.Substring(0, p.hgt.Length - 2);
+
+            if(int.TryParse(number, out int height))
+            {
+                if (unit == "cm")
+                {
+                    return height >= 150 && height <= 193;
+                }
+                if (unit == "in")
+                {
+                    return height >= 59 && height <= 76;
+                }
+            }
+            return false;
+        };
+
+        public static Func<Passport, bool> HairColourValid = (p) =>
+        {
+            var hairColour = p.hcl;
+
+            if (hairColour.Length == 7)
+            {
+                if (hairColour[0] == '#')
+                {
+                    return int.TryParse(hairColour.Substring(1), System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out var _);
+                }
+            }
+            return false;
+        };
+
+        public static Func<Passport, bool> EyeColourValid = (p) =>
+        {
+            var eyeColour = p.ecl;
+            switch(eyeColour)
+            {
+                case "amb":
+                case "blu":
+                case "brn":
+                case "gry":
+                case "grn":
+                case "hzl":
+                case "oth":
+                    return true;
+                default:
+                    return false;
+            }
+        };
+
+        public static Func<Passport, bool> PassportIdValid = (p) =>
+        {
+            if (p.pid.Length == 9)
+            {
+                return int.TryParse(p.pid, out int _);
+            }
+            return false;
+        };
 
         public long B(List<Passport> inputs)
         {
-            return -1;
+            return inputs.Where(FieldsPresentFunc)
+                .Where(PassportIdValid)
+                .Where(EyeColourValid)
+                .Where(HairColourValid)
+                .Where(HeightValid)
+                .Where(BirthYearValid)
+                .Where(IssueYearValid)
+                .Where(ExpiryYearValid)
+                .Count();
         }
 
         private IEnumerable<string> GroupsUntilWhiteSpace(string[] inputs)
@@ -94,15 +186,5 @@ namespace AdventOfCode2020
         public string ecl; //Eye Colour
         public string pid; //Passport Id
         public string cid; //Country Id
-
-        public bool IsValid => !string.IsNullOrWhiteSpace(byr)
-            && !string.IsNullOrWhiteSpace(iyr)
-            && !string.IsNullOrWhiteSpace(eyr)
-            && !string.IsNullOrWhiteSpace(hgt)
-            && !string.IsNullOrWhiteSpace(hcl)
-            && !string.IsNullOrWhiteSpace(ecl)
-            && !string.IsNullOrWhiteSpace(pid)
-            //&& !string.IsNullOrWhiteSpace(cid)
-            ;
     }
 }
--- 2020/AdventOfCode2020Tests/UnitTest.cs
+++ 2020/AdventOfCode2020Tests/UnitTest.cs
@@ -24,7 +24,7 @@ namespace AdventOfCode2020Tests
             };
 
             var inputs = day.SetupInputs(testCase);
-            if (partOne)
+            if(partOne)
                 Assert.Equal(514579, day.A(inputs));
             else
                 Assert.Equal(241861950, day.B(inputs));
@@ -46,7 +46,7 @@ namespace AdventOfCode2020Tests
 
             var inputs = day.SetupInputs(testCase);
 
-            if (partOne)
+            if(partOne)
                 Assert.Equal(2, day.A(inputs));
             else
                 Assert.Equal(1, day.B(inputs));
@@ -82,10 +82,8 @@ namespace AdventOfCode2020Tests
                 Assert.Equal(336, day.B(inputs));
         }
 
-        [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public void Day4_Test(bool partOne)
+        [Fact]
+        public void Day4_Part1_Test()
         {
             Day4 day = new Day4();
 
@@ -108,10 +106,83 @@ namespace AdventOfCode2020Tests
 
             var inputs = day.SetupInputs(testCase);
 
-            if(partOne)
-                Assert.Equal(2, day.A(inputs));
-            else
-                Assert.Equal(336, day.B(inputs));
+            Assert.Equal(2, day.A(inputs));
         }
+
+        [Fact]
+        public void Day4_Func_Test()
+        {
+            Assert.True(Day4.BirthYearValid(new Passport { byr = "2002" }));
+            Assert.False(Day4.BirthYearValid(new Passport { byr = "2003" }));
+
+            Assert.True(Day4.HeightValid(new Passport { hgt = "60in" }));
+            Assert.True(Day4.HeightValid(new Passport { hgt = "190cm" }));
+            Assert.False(Day4.HeightValid(new Passport { hgt = "190in" }));
+            Assert.False(Day4.HeightValid(new Passport { hgt = "190" }));
+
+            Assert.True(Day4.HairColourValid(new Passport { hcl = "#123abc" }));
+            Assert.False(Day4.HairColourValid(new Passport { hcl = "#123abz" }));
+            Assert.False(Day4.HairColourValid(new Passport { hcl = "123abc" }));
+
+            Assert.True(Day4.EyeColourValid(new Passport { ecl = "brn" }));
+            Assert.False(Day4.EyeColourValid(new Passport { ecl = "wat" }));
+
+            Assert.True(Day4.PassportIdValid(new Passport { pid = "000000001" }));
+            Assert.False(Day4.PassportIdValid(new Passport { pid = "0123456789" }));
+        }
+
+        [Fact]
+        public void Day4_Part2_Invalid_Test()
+        {
+            Day4 day = new Day4();
+
+            string[] invalidPassports = new[]
+            {
+                "eyr:1972 cid:",
+                "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+                "",
+                "iyr:2019",
+                "hcl:#602927 eyr:1967 hgt:170cm",
+                "ecl:grn pid:012533040 byr:1946",
+                "",
+                "hcl:dab227 iyr:2012",
+                "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+                "",
+                "hgt:59cm ecl:zzz",
+                "eyr:2038 hcl:74454a iyr:2023",
+                "pid:3556412378 byr:2007",
+            };
+
+            var inputs = day.SetupInputs(invalidPassports);
+
+            Assert.Equal(0, day.B(inputs));
+        }
+
+        [Fact]
+        public void Day4_Part2_Valid_Test()
+        {
+            Day4 day = new Day4();
+
+            string[] validPassports = new[]
+            {
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+                "hcl:#623a2f",
+                "",
+                "eyr:2029 ecl:blu cid:129 byr:1989",
+                "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+                "",
+                "hcl:#888785",
+                "hgt:164cm byr:2001 iyr:2015 cid:88",
+                "pid:545766238 ecl:hzl",
+                "eyr:2022",
+                "",
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+            };
+
+            var inputs = day.SetupInputs(validPassports);
+
+            Assert.Equal(4, day.B(inputs));
+        }
+
     }
 }
