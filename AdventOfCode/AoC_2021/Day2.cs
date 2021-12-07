49310aec0eb54ed314e1703e5e11b15e9d34acab
Ced <cbourneville@gmail.com>
2021-12-07 21:15:51 +1300

    Day2 scaffold

---
+++ AdventOfCode/AoC_2021/Day2.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCodeCore;
+
+namespace AoC_2021
+{
+    public class Day2 : IDay<double>
+    {
+        public long A(List<double> inputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long B(List<double> inputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<double> SetupInputs(string[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
