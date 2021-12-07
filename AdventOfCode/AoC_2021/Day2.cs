21fb39499b1285c515572b4864507340ba91084c
Ced <cbourneville@gmail.com>
2021-12-07 21:25:29 +1300

    Day2 inputs and part A

--- AdventOfCode/AoC_2021/Day2.cs
+++ AdventOfCode/AoC_2021/Day2.cs
@@ -2,21 +2,39 @@
 
 namespace AoC_2021
 {
-    public class Day2 : IDay<double>
+    public class Day2 : IDay<(string, int)>
     {
-        public long A(List<double> inputs)
+        public long A(List<(string, int)> inputs)
+        {
+            var coords = (X: 0, Y: 0);
+
+            foreach (var command in inputs)
+            {
+                switch (command.Item1)
+                {
+                    case "forward":
+                        coords.X += command.Item2;
+                        break;
+                    case "down":
+                        coords.Y += command.Item2;
+                        break;
+                    case "up":
+                        coords.Y += command.Item2;
+                        break;
+                }
+            }
+
+            return coords.X * coords.Y;
+        }
+
+        public long B(List<(string, int)> inputs)
         {
             throw new NotImplementedException();
         }
 
-        public long B(List<double> inputs)
+        public List<(string, int)> SetupInputs(string[] inputs)
         {
-            throw new NotImplementedException();
-        }
-
-        public List<double> SetupInputs(string[] inputs)
-        {
-            throw new NotImplementedException();
+            return inputs.Select(x => (x.Split(' ')[0], int.Parse(x.Split(' ')[1]))).ToList();
         }
     }
 }
