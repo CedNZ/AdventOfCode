2aa076ca7e33bcfd9c17a1a7dbc781180892bb3e
Ced <cbourneville@gmail.com>
2021-12-07 21:15:15 +1300

    2021 project, Day1 complete

---
+++ AdventOfCode/AoC_2021/AoC_2021.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCodeCore;
+
+namespace AoC_2021
+{
+    public class AoC_2021 : Year
+    {
+        public AoC_2021(DayRunner dayRunner) 
+            : base(dayRunner)
+        {
+        }
+
+        public override IEnumerable<DayResult> RunAll()
+        {
+            for (int i = 1; i < 2; i++)
+            {
+                yield return RunDay(i);
+            }
+        }
+
+        public override DayResult RunDay(int dayNum)
+        {
+            return dayNum switch
+            {
+                1 => RunDay(dayNum, () => new Day1()),
+                2 => RunDay(dayNum, () => new Day2()),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
---
+++ AdventOfCode/Tests/Tests_2021.cs
@@ -0,0 +1,45 @@
+using AdventOfCodeCore;
+using AoC_2021;
+using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+
+namespace Tests
+{
+    public class Tests_2021
+    {
+        [Theory]
+        [ClassData(typeof(DayTestData))]
+        public void DayTest<TIn, TOut>(string inputFile, Func<IDayOut<TIn, TOut>> GetDay, TOut expectedAnswer, bool partOne)
+        {
+            IDayOut<TIn, TOut> day = GetDay();
+
+            var inputs = day.SetupInputs(System.IO.File.ReadAllLines($"..\\..\\..\\TestInput\\day{inputFile}.txt"));
+
+            TOut result;
+            if (partOne)
+            {
+                result = day.A(inputs);
+            }
+            else
+            {
+                result = day.B(inputs);
+            }
+
+            result.Should().Be(expectedAnswer);
+        }
+    }
+
+    public class DayTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "1", () => new Day1(), 7, true };
+            yield return new object[] { "1", () => new Day1(), 5, false };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
