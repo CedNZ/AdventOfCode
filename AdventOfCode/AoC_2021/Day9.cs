e90be0dd2edc12ffe13ac09764550296195d0bbe
Ced <cbourneville@gmail.com>
2021-12-09 22:08:24 +1300

    Day 9 de duplicate

--- AdventOfCode/AoC_2021/Day9.cs
+++ AdventOfCode/AoC_2021/Day9.cs
@@ -13,6 +13,13 @@ namespace AoC_2021
         {
             List<((int, int), int)> lowPoints = new();
 
+            FindLowPoints(inputs, lowPoints);
+
+            return lowPoints.Sum(x => x.Item2 + 1);
+        }
+
+        private static void FindLowPoints(List<int[]> inputs, List<((int, int), int)> lowPoints)
+        {
             for (int i = 0; i < inputs.Count; i++)
             {
                 var previousRow = i > 0 ? inputs[i - 1] : Enumerable.Repeat(9, inputs[i].Count()).ToArray();
@@ -36,9 +43,9 @@ namespace AoC_2021
                     }
                 }
             }
-
-            return lowPoints.Sum(x => x.Item2 + 1);
         }
+
+
 
         List<int[]> heightMap = new();
         List<(int, int)> basin = new();
@@ -48,29 +55,7 @@ namespace AoC_2021
             heightMap = inputs.ToList();
             List<((int, int), int)> lowPoints = new();
 
-            for (int i = 0; i < inputs.Count; i++)
-            {
-                var previousRow = i > 0 ? inputs[i - 1] : Enumerable.Repeat(9, inputs[i].Count()).ToArray();
-                var currentRow = inputs[i];
-                var nextRow = i < inputs.Count - 1 ? inputs[i + 1] : Enumerable.Repeat(9, inputs[i].Count()).ToArray();
-
-                for (int j = 0; j < currentRow.Count(); j++)
-                {
-                    var cell = currentRow[j];
-                    var north = previousRow[j];
-                    var south = nextRow[j];
-                    var east = j < currentRow.Count() - 1 ? currentRow[j + 1] : 9;
-                    var west = j > 0 ? currentRow[j - 1] : 9;
-
-                    if (cell < north
-                        && cell < south
-                        && cell < east
-                        && cell < west)
-                    {
-                        lowPoints.Add(((i, j), cell));
-                    }
-                }
-            }
+            FindLowPoints(inputs, lowPoints);
 
             List<int> basinSize = new();
 
