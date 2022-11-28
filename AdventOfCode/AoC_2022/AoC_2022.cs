7346190c645e17c95eccb5320935ecc8d0214444
Ced <cbourneville@gmail.com>
2022-11-29 09:35:52 +1300

    2022 initial set-up, and 2018 testing for downloading input and submitting

---
+++ AdventOfCode/AoC_2022/AoC_2022.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCodeCore;
+
+namespace AoC_2022
+{
+    public class AoC_2022 : Year
+    {
+        public AoC_2022(DayRunner dayRunner) 
+            : base(dayRunner)
+        {
+        }
+
+        public override async Task<DayResult> RunDayAsync(int day)
+        {
+            //var dayFunc = day switch
+            //{
+            //    1 => () => day,
+            //    _ => () => day
+            //};
+            //return RunDay(day, null);
+            throw new NotImplementedException();
+        }
+    }
+}
