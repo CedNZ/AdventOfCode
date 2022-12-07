e73fe0bd35346bf92621eb5be62b676654b0106e
Ced <cbourneville@gmail.com>
2022-12-07 22:24:16 +1300

    Day7 more wip

--- AdventOfCode/AoC_2022/Day7.cs
+++ AdventOfCode/AoC_2022/Day7.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdventOfCodeCore;
 
 namespace AoC_2022
 {
-    internal class Day7
+    public class Day7 : IDay<string[]>
     {
+        public long A(List<string[]> inputs)
+        {
+            for (int i = 0; i < inputs.Count(); i++)
+            {
+                var line = inputs[i];
+
+                if (line[0] == "$")
+                {
+                    if ()
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        File root = new File
+        {
+            Files = new(),
+            Name = "root",
+            Size = 0,
+            Folder = null,
+        };
+
+        public long B(List<string> inputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string[]> SetupInputs(string[] inputs)
+        {
+            return inputs.Select(x => x.Split(' ')).ToList();
+        }
+
+        internal class File
+        {
+            public string Name { get; set; }
+            public int Size { get; set; }
+
+            public File Folder { get; set; }
+
+            public List<File> Files { get; set; }
+        }
     }
 }
